0a7ac6deda72bba0affc6bb226cf23f2436cec0b
erickhene <djefrisonferdinan@gmail>
2023-01-29 15:48:04 +0800

    Add Login Root

--- src/test/java/com/erickhene/SekolahApplicationTests.java
+++ src/test/java/com/erickhene/SekolahApplicationTests.java
@@ -1,13 +1,18 @@
 package com.erickhene;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SekolahApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.error("Error");
+		log.info("Info");
+		log.warn("Warn");
 	}
 
 }
